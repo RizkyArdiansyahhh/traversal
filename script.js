b529cba4e5787cbe689cb5719e920d9ddf9035b2
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-09-12 00:00:18 +0700

    update file script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const closes = document.querySelectorAll(".close");
+
+closes.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.parentElement.style.display = "none";
+  });
+});

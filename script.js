ff39f354b1a586e36ef8fb0a7d353408bdaf2d31
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-09-12 00:46:52 +0700

    event bubling di js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const closes = document.querySelectorAll(".close");
 
-closes.forEach(function (el) {
-  el.addEventListener("click", function (e) {
+// closes.forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.target.parentElement.style.display = "none";
+//     e.preventDefault();
+//     console.log(e);
+//   });
+// });
+
+// YANG EFEKTIF
+const container = document.querySelector(".container");
+
+container.addEventListener("click", function (e) {
+  if (e.target.className == "close") {
     e.target.parentElement.style.display = "none";
     e.preventDefault();
-    console.log(e);
-  });
+    e.stopPropagation();
+  }
 });

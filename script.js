d0a13b639e6cc1096bde3a6b332862de92b1cb03
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-09-12 00:26:37 +0700

    prefen default adalah method untuk menghentikan aksi default

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const closes = document.querySelectorAll(".close");
 closes.forEach(function (el) {
   el.addEventListener("click", function (e) {
     e.target.parentElement.style.display = "none";
+    e.preventDefault();
+    console.log(e);
   });
 });
